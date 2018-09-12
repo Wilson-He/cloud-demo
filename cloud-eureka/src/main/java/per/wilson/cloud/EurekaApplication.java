4b735d378e8fdab5272e23e3ffa05ebf4381725c
Wilson-He <845023508@qq.com>
2018-09-13 00:40:43 +0800

    exclude server config

--- cloud-eureka/src/main/java/per/wilson/cloud/EurekaApplication.java
+++ cloud-eureka/src/main/java/per/wilson/cloud/EurekaApplication.java
@@ -2,6 +2,7 @@ package per.wilson.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer
 @SpringBootApplication
+@EnableConfigServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
