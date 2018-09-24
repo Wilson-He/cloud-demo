bdd8d40062c192564576cd836ff79da9dcae4021
Wilson-He <845023508@qq.com>
2018-09-25 00:21:50 +0800

    update swagger config

--- cloud-docs/src/main/java/per/wilson/cloud/config/SwaggerResources.java
+++ cloud-docs/src/main/java/per/wilson/cloud/config/SwaggerResources.java
@@ -1,12 +1,16 @@
 package per.wilson.cloud.config;
 
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: hewx
@@ -15,20 +19,28 @@ import java.util.List;
  */
 @Component
 @Primary
+@ConfigurationProperties(prefix = "swagger.resources")
+@Setter
 public class SwaggerResources implements SwaggerResourcesProvider {
+    private Map<String, String> moduleName;
+    private Map<String, String> moduleDocsUrl;
 
-  @Override
-  public List<SwaggerResource> get() {
-    List<SwaggerResource> resources = new ArrayList<>();
-    resources.add(swaggerResource("用户提供者模块", "/user-provider/v2/api-docs", "latest"));
-    resources.add(swaggerResource("用户消费者模块", "/user-consumer/v2/api-docs", "latest"));
-    return resources;
-  }
+    @Override
+    public List<SwaggerResource> get() {
+        if(moduleName == null || moduleName.isEmpty()){
+            return Collections.EMPTY_LIST;
+        }
+        List<SwaggerResource> resources = new ArrayList<>();
+        moduleName.forEach((k,v) -> resources.add(swaggerResource(v,moduleDocsUrl.get(k))));
+        return resources;
+    }
 
-  private SwaggerResource swaggerResource(String name, String url, String version) {
-    SwaggerResource swaggerResource = new SwaggerResource();
-    swaggerResource.setName(name);
-    swaggerResource.setUrl(url);
-    return swaggerResource;
-  }
+    private SwaggerResource swaggerResource(String name, String url) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setUrl(url);
+        swaggerResource.setSwaggerVersion("latest");
+        return swaggerResource;
+    }
+
 }
