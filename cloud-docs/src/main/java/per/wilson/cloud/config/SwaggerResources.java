e2c004655d7ef61249639112138902884d921136
Wilson-He <845023508@qq.com>
2018-09-25 09:11:50 +0800

    update

--- cloud-docs/src/main/java/per/wilson/cloud/config/SwaggerResources.java
+++ cloud-docs/src/main/java/per/wilson/cloud/config/SwaggerResources.java
@@ -1,5 +1,9 @@
 package per.wilson.cloud.config;
 
+import java.util.Collections;
+import java.util.Map;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
@@ -14,20 +18,29 @@ import java.util.List;
  * @since:
  */
 @Component
+@Primary
+@ConfigurationProperties(prefix = "swagger.resources")
+@Setter
 public class SwaggerResources implements SwaggerResourcesProvider {
+    private Map<String, String> moduleName;
+    private Map<String, String> moduleDocsUrl;
 
-  @Override
-  public List<SwaggerResource> get() {
-    List<SwaggerResource> resources = new ArrayList<>();
-    resources.add(swaggerResource("用户提供者模块", "/user-provider/v2/api-docs"));
-    resources.add(swaggerResource("用户消费者模块", "/user-consumer/v2/api-docs"));
-    return resources;
-  }
+    @Override
+    public List<SwaggerResource> get() {
+        if(moduleName == null || moduleName.isEmpty()){
+            return Collections.EMPTY_LIST;
+        }
+        List<SwaggerResource> resources = new ArrayList<>();
+        moduleName.forEach((k,v) -> resources.add(swaggerResource(v,moduleDocsUrl.get(k))));
+        return resources;
+    }
 
-  private SwaggerResource swaggerResource(String name, String url) {
-    SwaggerResource swaggerResource = new SwaggerResource();
-    swaggerResource.setName(name);
-    swaggerResource.setUrl(url);
-    return swaggerResource;
-  }
+    private SwaggerResource swaggerResource(String name, String url) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setUrl(url);
+        swaggerResource.setSwaggerVersion("latest");
+        return swaggerResource;
+    }
+
 }
