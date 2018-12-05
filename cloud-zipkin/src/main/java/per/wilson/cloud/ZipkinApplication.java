1adbcb639c11284ffb1e19b0e81e7f9d03f471f7
Wilson <845023508@qq.com>
2018-12-05 11:05:31 +0800

    add zipkin

---
+++ cloud-zipkin/src/main/java/per/wilson/cloud/ZipkinApplication.java
@@ -0,0 +1,15 @@
+package per.wilson.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class);
+    }
+}
